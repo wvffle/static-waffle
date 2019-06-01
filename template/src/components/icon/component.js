594bd5955e497753f272d0eee1fe2335b21297f0
Kasper Seweryn <casper@wvffle.net>
2019-06-01 14:05:05 +0200

    Add color attribute to icon component

--- template/src/components/icon/component.js
+++ template/src/components/icon/component.js
@@ -5,6 +5,9 @@ export default {
     'eva-icons/style/eva-icons.min.css',
   ],
   js: {},
+  props: {
+    color: { type: String },
+  },
   functional: true,
   render (h, ctx) {
     const { text } = ctx.slots().default[0]
@@ -15,6 +18,9 @@ export default {
         vmiddle: true,
         [`eva-${text}`]: true,
         [ctx.data.staticClass]: true,
+      },
+      style: {
+        color: ctx.color,
       },
     })
   }
